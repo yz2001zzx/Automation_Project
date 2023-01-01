f48502ec5c4557e4c3cccd2cc0e9c5f659e884d9
Ubuntu <ubuntu@ip-172-31-86-26.ec2.internal>
2023-01-01 20:30:00 +0000

    added some additional features for the task 3

--- automation.sh
+++ automation.sh
@@ -4,8 +4,10 @@
 
 # Project: Automation Project 
 
-# This script has been validated and the execuation of all the operations is aligned with my expectation.
+# The task 2 part  has been validated and the execuation of all the operations is aligned with my expectation.
 # The tar file can be found in S3 Bucket after running this script
+
+# This script has addeded an additional feature to get the size of the tar file and create or append an inventory.html file
 
 ##################################################################################################
 #	                          Task 2
@@ -52,6 +54,10 @@ FileName=${myname}-httpd-logs-${timestamp}.tar
 
 tar -cvf $FileName /var/log/apache2/
 
+# Get the size of the tar file and save it to a variable (to be used for task 3)
+
+sizeOfTar=$(wc --bytes $FileName | awk '{print $1}')
+
 # Move this tar file into the /tmp/ directory
 
 mv $FileName /tmp
@@ -62,6 +68,34 @@ s3_bucket="upgrad-zixiangzhou"
 
 aws s3 cp /tmp/$FileName s3://${s3_bucket}/$FileName
 
+##################################################################################################
+#	                          Task 3
+##################################################################################################
 
+
+# 5. Check the presense of the inventory.html file in /var/www/html/;
+# If not found, we create it.
+
+# Specify the filename to check in the designated Directory
+
+File=/var/www/html/inventory.html
+
+# Check if the file already exists, if not, create one with the header row using html table format. 
+# if yes, append additional row to record a new entry in the inventory.html file
+
+if [ -f "$File" ]; then
+	echo "$File exists."
+	echo "<table width="500" cellspacing="12"><tr><td align="middle">httpd-logs</td><td align="middle">${timestamp}</td><td align="left">tar</td><td align="middle">${sizeOfTar}</td></tr></table>" >>$File
+else
+	echo "$File does not exist now we create a file."
+	sudo touch $File
+	sudo chmod 777 $File
+	echo "<table width="500" cellspacing="12"><tr><th>Log Type</th><th>Date Created</th><th>Type</th><th>Size</th></tr></table>" >>$File
+	echo "<table width="500" cellspacing="12"><tr><td align="middle">httpd-logs</td><td align="middle">${timestamp}</td><td align="left">tar</td><td align="middle">${sizeOfTar}</td></tr></table>" >>$File
+fi
+
+# Upload the inventory log to the AWS S3 (Not Required)
+
+aws s3 cp $File s3://${s3_bucket}/inventory.html
 
 
